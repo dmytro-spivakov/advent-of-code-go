044201d67e19c7997b0302cde35dc97a945cdb93
Dmytro Spivakov <dmytro_spivakov@proton.me>
2024-07-02 06:46:24 +0300

    chore: add day 3 puzzle description and input

--- advent.go
+++ advent.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"github.com/dmytro-spivakov/advent-of-code-go/day1"
 	"github.com/dmytro-spivakov/advent-of-code-go/day2"
+	"github.com/dmytro-spivakov/advent-of-code-go/day3"
 )
 
 func main() {
 	fmt.Printf("Day 1 solution: %d\n", day1.Solution())
 	fmt.Printf("Day 2 solution: %d\n", day2.Solution())
+	fmt.Printf("Day 3 solution: %d\n", day3.Solution())
 }
