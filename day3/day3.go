85be796f3217d798a7b3738ab1aaab8e80c2abda
Dmytro Spivakov <dmytro_spivakov@proton.me>
2024-07-03 05:17:07 +0300

    Day 3: part 1 done

--- day3/day3.go
+++ day3/day3.go
@@ -28,10 +28,125 @@ Here is an example engine schematic:
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+
+--- Part Two ---
+
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
+What is the sum of all of the gear ratios in your engine schematic?
 **/
 
 package day3
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func Solution() int {
-	return 0
+	inputFile, err := os.Open("./day3/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(inputFile)
+	var charsMatrix [][]string
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		charsMatrix = append(charsMatrix, strings.Split(currentLine, ""))
+	}
+
+	sum := 0
+	numberRegex := regexp.MustCompile(`[0-9]{1}`)
+	for y, row := range charsMatrix {
+		for x, rowItem := range row {
+			if !numberRegex.Match([]byte(rowItem)) {
+				continue
+			}
+
+			currentNumberCoords := []int{x}
+			x1 := x + 1
+			for x1 <= len(row)-1 && numberRegex.Match([]byte(row[x1])) {
+				currentNumberCoords = append(currentNumberCoords, x1)
+				x1 += 1
+			}
+			adjacentXCoords := make([]int, len(currentNumberCoords))
+			copy(adjacentXCoords, currentNumberCoords)
+			adjacentXCoords = append(adjacentXCoords, currentNumberCoords[0]-1)
+			adjacentXCoords = append(adjacentXCoords, currentNumberCoords[len(currentNumberCoords)-1]+1)
+			adjacentCoords := [][2]int{}
+			for _, xCoord := range adjacentXCoords {
+				if xCoord < 0 || xCoord > len(row)-1 {
+					continue
+				}
+
+				if y-1 >= 0 {
+					adjacentCoords = append(adjacentCoords, [2]int{y - 1, xCoord})
+				}
+				adjacentCoords = append(adjacentCoords, [2]int{y, xCoord})
+				if y+1 <= len(charsMatrix)-1 {
+					adjacentCoords = append(adjacentCoords, [2]int{y + 1, xCoord})
+				}
+			}
+
+			addCurrentNum := false
+			for _, coordPair := range adjacentCoords {
+				currentSubStr := charsMatrix[coordPair[0]][coordPair[1]]
+
+				if currentSubStr != "." && !numberRegex.Match([]byte(currentSubStr)) {
+					addCurrentNum = true
+					break
+				}
+			}
+
+			if addCurrentNum {
+				numberChars := []string{}
+				for _, numberXCoord := range currentNumberCoords {
+					numberChars = append(numberChars, charsMatrix[y][numberXCoord])
+					charsMatrix[y][numberXCoord] = "."
+				}
+
+				numberStr := strings.Join(numberChars, "")
+				number, err := strconv.ParseInt(numberStr, 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				sum += int(number)
+			}
+
+		}
+	}
+
+	err = inputFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return sum
 }
