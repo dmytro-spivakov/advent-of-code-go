5647dff49fcbbbb98c1e6d209ec1384ca782cf22
Dmytro Spivakov <dmytro_spivakov@proton.me>
2024-07-04 06:14:09 +0300

    chore: add day 4 blank package, update README

--- advent.go
+++ advent.go
@@ -5,10 +5,12 @@ import (
 	"github.com/dmytro-spivakov/advent-of-code-go/day1"
 	"github.com/dmytro-spivakov/advent-of-code-go/day2"
 	"github.com/dmytro-spivakov/advent-of-code-go/day3"
+	"github.com/dmytro-spivakov/advent-of-code-go/day4"
 )
 
 func main() {
 	fmt.Printf("Day 1 solution: %d\n", day1.Solution())
 	fmt.Printf("Day 2 solution: %d\n", day2.Solution())
 	fmt.Printf("Day 3 solution: %d\n", day3.Solution())
+	fmt.Printf("Day 4 solution: %d\n", day4.Solution())
 }
