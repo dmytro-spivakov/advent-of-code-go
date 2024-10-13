3d9468dae3cf6f60bcbde734d0da006e576f45f9
Dmytro Spivakov <dmytro_spivakov@proton.me>
2024-10-13 05:47:28 +0300

    Day 7: part 1 wip

---
+++ day07/day07_test.go
@@ -0,0 +1,21 @@
+package day07
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolution1(t *testing.T) {
+	cases := map[string]int{
+		"test_input11": 6440,
+	}
+
+	for inputFile, expectedResult := range cases {
+		result := Solution1(inputFile)
+		if result != expectedResult {
+			t.Fatalf("Solution1() = %d, expecting %d\n", result, expectedResult)
+		} else {
+			fmt.Printf("Solution1() = %d, OK\n", result)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"advent-of-code/day04"
 	"advent-of-code/day05"
 	"advent-of-code/day06"
+	"advent-of-code/day07"
 	"fmt"
 )
 
@@ -28,4 +29,7 @@ func main() {
 
 	fmt.Printf("Day 6, solution 1: %v\n", day06.Solution1("inputs/day06"))
 	fmt.Printf("Day 6, solution 2: %v\n", day06.Solution2("inputs/day06"))
+
+	fmt.Printf("Day 7, solution 1: %v\n", day07.Solution1("inputs/day07"))
+	fmt.Printf("Day 7, solution 2: %v\n", day07.Solution2("inputs/day07"))
 }
