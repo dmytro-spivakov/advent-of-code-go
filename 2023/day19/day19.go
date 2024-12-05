44a385fc3081e0fb794135ba72ee3f6502f517a9
Dmytro Spivakov <dmytro_spivakov@proton.me>
2024-12-05 02:19:57 +0200

    Day 19: part 1

--- 2023/day19/day19.go
+++ 2023/day19/day19.go
@@ -6,94 +6,75 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"slices"
 	"strconv"
 	"strings"
 )
 
-type Part struct {
-	x                int
-	m                int
-	a                int
-	s                int
-	appliedWorkflows []string
-	workflowStep     string
+type Workflow struct {
+	name     string
+	fallback string
+	rules    []Rule
 }
 
-func (p *Part) getVal(field string) int {
-	switch field {
-	case "x":
-		return (*p).x
-	case "m":
-		return (*p).m
-	case "a":
-		return (*p).a
-	case "s":
-		return (*p).s
-	}
-
-	log.Fatalf("Part.getVal() unknown field %s\n", field)
-	return -1
-}
-
-func (p *Part) isProcessed() bool {
-	if ws := (*p).workflowStep; ws == "A" || ws == "R" {
-		return true
-	}
-	return false
-}
-
-func (p *Part) rating() int {
-	return p.x + p.m + p.a + p.s
-}
-
-type RuleSet struct {
-	name          string
-	rawInput      string
-	workflowSteps []func(p Part) (bool, string)
-}
-
-func (rs *RuleSet) apply(p *Part) bool {
-	for _, wfStep := range rs.workflowSteps {
-		ok, nextStep := wfStep(*p)
-		if ok && !slices.Contains(p.appliedWorkflows, nextStep) {
-			fmt.Printf("PART: %v, NEXT STEP: %v\n", p, nextStep)
-			(*p).appliedWorkflows = append((*p).appliedWorkflows, p.workflowStep)
-			(*p).workflowStep = nextStep
-			return true
-		}
-
-	}
-	return false
+type Rule struct {
+	field       string
+	greaterThan bool
+	threshold   int
+	next        string
 }
 
 func Solution1(filepath string) int {
-	rss, p := parseInput(filepath)
-	fmt.Printf("RuleSet: len=%d, val=%v\n", len(rss), rss)
-	fmt.Printf("Part: len=%d, val=%v\n", len(p), p)
+	parts, workflows := parseInput(filepath)
+	fmt.Println("Parts:")
+	for _, p := range parts {
+		fmt.Println(p)
+	}
+	fmt.Println("-------------------")
+	fmt.Println("Workflows:")
+	for _, wf := range workflows {
+		fmt.Println(wf)
+	}
+	fmt.Println("-------------------")
+
+	wfMap := make(map[string]Workflow)
+	for _, wf := range workflows {
+		wfMap[wf.name] = wf
+	}
 
 	result := 0
-	for len(p) > 0 {
-		fmt.Printf("LEN=%v\n", len(p))
-		currentPart := &p[0]
-		fmt.Printf("currentPart=%v\n", currentPart)
-		for _, rs := range rss {
-			ok := rs.apply(currentPart)
-			if ok {
-				break
+	for _, p := range parts {
+		var history []string
+
+		currentStep := "in"
+		for currentStep != "A" && currentStep != "R" {
+			wf := wfMap[currentStep]
+
+			pass := false
+			var nextStep string
+			for _, r := range wf.rules {
+				if (r.greaterThan && p[r.field] > r.threshold) || (!r.greaterThan && p[r.field] < r.threshold) {
+					pass = true
+					nextStep = r.next
+					break
+				}
 			}
+
+			history = append(history, currentStep)
+
+			if !pass {
+				nextStep = wf.fallback
+			}
+			currentStep = nextStep
 		}
 
-		if currentPart.isProcessed() {
-			// fmt.Printf("currentPart=%v is processed\n", currentPart)
-			if currentPart.workflowStep == "A" {
-				result += currentPart.rating()
-			}
-			curLen := len(p)
-			p[0] = p[curLen-1]
-			p = p[:curLen-1]
+		history = append(history, currentStep)
+		fmt.Println(strings.Join(history, "->"))
+		if currentStep == "A" {
+			result += p["x"] + p["m"] + p["a"] + p["s"]
 		}
+
 	}
+
 	return result
 }
 
@@ -101,9 +82,9 @@ func Solution2(filepath string) int {
 	return -1
 }
 
-func parseInput(filepath string) ([]RuleSet, []Part) {
-	var ruleSets []RuleSet
-	var parts []Part
+func parseInput(filepath string) ([]map[string]int, []Workflow) {
+	var parts []map[string]int
+	var workflows []Workflow
 
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -120,67 +101,55 @@ func parseInput(filepath string) ([]RuleSet, []Part) {
 		if strings.HasPrefix(currentLine, "{") {
 			parts = append(parts, parsePart(currentLine))
 		} else {
-			ruleSets = append(ruleSets, parseRuleSet(currentLine))
+			workflows = append(workflows, parseWorkflow(currentLine))
 		}
 	}
 	if err = scanner.Err(); err != nil {
 		log.Fatalf("Error during input file read: %v\n", err.Error())
 	}
 
-	return ruleSets, parts
+	return parts, workflows
 }
 
-func parseRuleSet(rawInput string) RuleSet {
-	name := strings.Split(rawInput, "{")[0]
-	ruleSet := RuleSet{
+func parseWorkflow(rawInput string) Workflow {
+	wfParts := strings.Split(rawInput[:len(rawInput)-1], "{")
+	name := wfParts[0]
+	rest := strings.Split(wfParts[1], ",")
+	workflow := Workflow{
 		name:     name,
-		rawInput: rawInput,
+		fallback: rest[len(rest)-1],
 	}
-
-	ruleSegRegex := regexp.MustCompile(`\{(.+)\}`)
-	ruleSegStr := ruleSegRegex.FindStringSubmatch(rawInput)[1]
-	ruleSegments := strings.Split(ruleSegStr, ",")
-	var rules []func(p Part) (bool, string)
-	for _, ruleSeg := range ruleSegments {
-		// samples:
-		// a<10:gd - conditinal transition
-		// A       - unconditional transition
-		if !strings.Contains(ruleSeg, ":") {
-			rules = append(rules, func(p Part) (bool, string) { return true, ruleSeg })
-			continue
-		}
-
-		segParts := strings.Split(ruleSeg, ":")
-		if len(segParts) != 2 {
-			log.Fatalf("Failed to parse segment rule %s\n", ruleSeg)
-		}
-
-		endState := segParts[1]
-
-		condition := []rune(segParts[0])
-		conditionField := string(condition[0])                // a, m, s, x
-		greaterThan := condition[1] == '>'                    // always < or >
-		conditionThreshold := parseInt(string(condition[2:])) // whatever is left is the number to compare against
-
-		// fmt.Printf("ruleSeg=%v, endState=%v, conditionFiled=%v, greaterThan=%v, conditionThreshold=%v\n", ruleSeg, endState, conditionField, greaterThan, conditionThreshold)
-
+	rest = rest[:len(rest)-1]
+	for _, ruleStr := range rest {
+		parts := strings.Split(ruleStr, ":")
+		next := parts[1]
+		greaterThan := strings.Contains(parts[0], ">")
+		var field string
+		var threshold int
 		if greaterThan {
-			rules = append(
-				rules,
-				func(p Part) (bool, string) { ok := p.getVal(conditionField) > conditionThreshold; return ok, endState },
-			)
+			split := strings.Split(parts[0], ">")
+			field = split[0]
+			threshold = parseInt(split[1])
 		} else {
-			rules = append(
-				rules,
-				func(p Part) (bool, string) { ok := p.getVal(conditionField) < conditionThreshold; return ok, endState },
-			)
+			split := strings.Split(parts[0], "<")
+			field = split[0]
+			threshold = parseInt(split[1])
 		}
+		workflow.rules = append(
+			workflow.rules,
+			Rule{
+				field:       field,
+				greaterThan: greaterThan,
+				threshold:   threshold,
+				next:        next,
+			},
+		)
 	}
-	ruleSet.workflowSteps = rules
-	return ruleSet
+
+	return workflow
 }
 
-func parsePart(rawInput string) Part {
+func parsePart(rawInput string) map[string]int {
 	numRegex := regexp.MustCompile(`\d+`)
 	matches := numRegex.FindAllString(rawInput, -1)
 	if len(matches) != 4 {
@@ -188,12 +157,11 @@ func parsePart(rawInput string) Part {
 	}
 
 	// fields are always specified in the same order in the input
-	return Part{
-		x:            parseInt(matches[0]),
-		m:            parseInt(matches[1]),
-		a:            parseInt(matches[2]),
-		s:            parseInt(matches[3]),
-		workflowStep: "in",
+	return map[string]int{
+		"x": parseInt(matches[0]),
+		"m": parseInt(matches[1]),
+		"a": parseInt(matches[2]),
+		"s": parseInt(matches[3]),
 	}
 }
 
